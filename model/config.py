fe80840dafd7d01068ba7aa95c7984998fe58fe4
zenmezhemecaia <80655365+zenmezhemecaia@users.noreply.github.com>
2025-09-21 10:57:03 +0800

    Update config.py

--- model/config.py
+++ model/config.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 hidden_size = 128  # Latent dim
 interact_size = 128
-num_heads = 4  # Number of attention heads
-num_layers = 2  # Number of attention layers
+num_heads = 8  # Number of attention heads
+num_layers = 1  # Number of attention layers
 num_gnn_layers = 1  # GNN depth
 dropout_rate = 0.1  # Dropout rate p
 leakey = 0.1  # Hyperparameter of LeakyReLU
